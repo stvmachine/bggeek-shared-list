dc5dc0eb9369399447ef28bb2c301d9914a11749
Esteban Campos <campos.esteban@gmail.com>
2021-05-06 17:02:40 +1000

    added some filters

--- src/pages/collection.tsx
+++ src/pages/collection.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NextPage } from "next";
 import {
+  Stack,
   Wrap,
   WrapItem,
   Image,
@@ -11,7 +12,14 @@ import {
   ListItem,
   LinkBox,
   LinkOverlay,
+  Select,
+  InputLeftElement,
+  InputGroup,
+  Input,
 } from "@chakra-ui/react";
+
+import { SearchIcon } from "@chakra-ui/icons";
+
 import { getBggCollection, BggCollectionResponse } from "bgg-xml-api-client";
 
 import Footer from "../components/Footer";
@@ -78,6 +86,39 @@ export async function getStaticProps() {
   };
 }
 
+const COLLECTION_STATUS = [
+  "fortrade",
+  "own",
+  "preordered",
+  "prevowned",
+  "want",
+  "wanttobuy",
+  "wanttoplay",
+  "wishlist",
+];
+
+const numberOfPlayersOptions = [
+  { value: 1, name: 1 },
+  { value: 2, name: 2 },
+  { value: 3, name: 3 },
+  { value: 4, name: 4 },
+  { value: 5, name: 5 },
+  { value: 6, name: 6 },
+  { value: 7, name: 7 },
+  { value: 8, name: 8 },
+  { value: 9, name: 9 },
+  { value: 10, name: 10 },
+];
+
+const playingTimeOptions = [
+  { value: 1, name: "< 30 min" },
+  { value: 2, name: "30min - 1h" },
+  { value: 3, name: "1-2h" },
+  { value: 4, name: "2-3h" },
+  { value: 5, name: "3-4h" },
+  { value: 6, name: ">4h" },
+];
+
 const Index: NextPage<CollectionPageProps> = ({
   members,
   collectionGroupData,
@@ -86,10 +127,45 @@ const Index: NextPage<CollectionPageProps> = ({
     <Container height="100vh" maxWidth="100%">
       <Navbar />
 
+      <InputGroup width="xs">
+        <InputLeftElement
+          pointerEvents="none"
+          children={<SearchIcon color="gray.300" />}
+        />
+        <Input placeholder="Search" />
+      </InputGroup>
+
+      <Wrap>
+        <Select placeholder="Number of players" width="xxs">
+          {numberOfPlayersOptions &&
+            numberOfPlayersOptions.map((item) => (
+              <option
+                value={item.value}
+                key={`number_of_players_${item.value}`}
+              >
+                {item.name}
+              </option>
+            ))}
+        </Select>
+
+        <Select placeholder="Playing time" width="xxs">
+          {playingTimeOptions &&
+            playingTimeOptions.map((item) => (
+              <option
+                value={item.value}
+                key={`playing_time_${item.value}`}
+              >
+                {item.name}
+              </option>
+            ))}
+        </Select>
+      </Wrap>
+
       <Container mt={10} maxWidth="80%">
         {members && (
           <Heading fontSize={"3xl"} mb={10}>
-            Displaying {collectionGroupData.totalitems} games owned for the following members:
+            Displaying {collectionGroupData.totalitems} games owned for the
+            following members:
             <UnorderedList>
               {members.map((member, index) => (
                 <ListItem key={`${member}_${index}`}>
