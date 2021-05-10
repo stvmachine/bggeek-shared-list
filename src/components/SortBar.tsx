fa07e5ca7d525bf8943ec8a10041fe7b2c1b1af2
Esteban Campos <campos.esteban@gmail.com>
2021-05-10 14:57:51 +1000

    updated order by

--- src/components/SortBar.tsx
+++ src/components/SortBar.tsx
@@ -1,33 +1,39 @@
 import React from "react";
-import { HStack, Select } from "@chakra-ui/react";
+import { HStack, Select, FormControl, FormLabel } from "@chakra-ui/react";
 import { useFormContext } from "react-hook-form";
 
 const SortBar = () => {
   const { register } = useFormContext();
 
   return (
-    <HStack justifyContent="flex-end">
-      <Select placeholder="Order by" {...register("orderBy")} width="xxs">
-        <option value="name_asc" key="order_by_name_asc">
-          Name: A-Z
-        </option>
-        <option value="name_desc" key="order_by_name_desc">
-          Name: Z-A
-        </option>
-        <option value="rating_asc" key="order_by_rating_asc">
-          Rating (asc)
-        </option>
-        <option value="rating_desc" key="order_by_rating_desc">
-          Rating (desc)
-        </option>
-        <option value="year_asc" key="order_by_year_asc">
-          Year (asc)
-        </option>
-        <option value="year_desc" key="order_by_year_desc">
-          Year (desc)
-        </option>
-      </Select>
-    </HStack>
+    <FormControl id="orderBy-wrapper">
+      <HStack justifyContent="flex-end" alignItems="center">
+        <FormLabel fontSize="md" mt={2}>Sort: </FormLabel>
+        <Select
+          {...register("orderBy")}
+          width="xxs"
+        >
+          <option value="name_asc" key="order_by_name_asc">
+            Name: A-Z
+          </option>
+          <option value="name_desc" key="order_by_name_desc">
+            Name: Z-A
+          </option>
+          <option value="rating_asc" key="order_by_rating_asc">
+            Rating: Lowest first
+          </option>
+          <option value="rating_desc" key="order_by_rating_desc">
+            Rating: Highest first
+          </option>
+          <option value="year_asc" key="order_by_year_asc">
+            Year: Oldest first
+          </option>
+          <option value="year_desc" key="order_by_year_desc">
+            Year: Newest first
+          </option>
+        </Select>
+      </HStack>
+    </FormControl>
   );
 };
 
