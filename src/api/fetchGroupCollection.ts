e246d8924071902188ef213c8848c4c90e55f4d6
Esteban Campos <campos.esteban@gmail.com>
2021-05-17 11:55:31 +1000

    fixing the use of react-query

--- src/api/fetchGroupCollection.ts
+++ src/api/fetchGroupCollection.ts
@@ -1,40 +1,45 @@
 import { BggCollectionResponse, getBggCollection } from "bgg-xml-api-client";
 import { ICollection, IItem } from "../utils/types";
 
-export const fetchGroupCollection = async (
+export const fetchCollections = async (
   usernames: string[]
-): Promise<{ boardgames: IItem[]; collections: ICollection[] }> => {
-  const rawData = await Promise.all(
-    usernames.map(async (username) => {
-      const collectionResponse = await getBggCollection({
-        own: 1,
-        subtype: "boardgame",
-        stats: 1,
-        username,
-      });
+): Promise<ICollection[]> =>
+  Promise.all(usernames.map((username) => fetchCollection(username)));
 
-      return {
-        ...collectionResponse.data,
-        item: collectionResponse.data.item.map((item: IItem) => ({
-          yearpublished: item.yearpublished,
-          stats: item.stats,
-          subtype: item.subtype,
-          objectid: item.objectid,
-          thumbnail: item.thumbnail,
-          name: { ...item.name },
-          owners: [
-            {
-              username,
-              status: item.status,
-              collid: item.collid,
-            },
-          ],
-        })),
-        username,
-      };
-    })
-  );
+export const fetchCollection = async (
+  username: string
+): Promise<ICollection> => {
+  const collectionResponse = await getBggCollection({
+    own: 1,
+    subtype: "boardgame",
+    stats: 1,
+    username,
+  });
 
+  return {
+    ...collectionResponse.data,
+    item: collectionResponse.data.item.map((item: IItem) => ({
+      yearpublished: item.yearpublished,
+      stats: item.stats,
+      subtype: item.subtype,
+      objectid: item.objectid,
+      thumbnail: item.thumbnail,
+      name: { ...item.name },
+      owners: [
+        {
+          username,
+          status: item.status,
+          collid: item.collid,
+        },
+      ],
+    })),
+    username,
+  } as ICollection;
+};
+
+export const mergeCollections = (
+  rawData: ICollection[]
+): { boardgames: IItem[]; collections: ICollection[] } => {
   const collections = rawData.map(({ totalitems, pubdate, username }: any) => ({
     totalitems,
     pubdate,
@@ -66,7 +71,7 @@ export const fetchGroupCollection = async (
     );
 
   return {
-    collections,
-    boardgames: Object.values(boardgames) as IItem[],
+    collections: collections,
+    boardgames: Object.values(boardgames),
   };
 };
--- src/pages/collection.tsx
+++ src/pages/collection.tsx
@@ -2,42 +2,51 @@ import React, { useMemo } from "react";
 import { useForm, FormProvider } from "react-hook-form";
 import { NextPage } from "next";
 import { Box, Container, Stack } from "@chakra-ui/react";
-import { useQuery } from "react-query";
+import { useQueries } from "react-query";
 
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import { IItem, ICollection } from "../utils/types";
+import { ICollection } from "../utils/types";
 import SearchSidebar from "../components/SearchSidebar";
 import Results from "../components/Results";
 import SortBar from "../components/SortBar";
-import { fetchGroupCollection } from "../api/fetchGroupCollection";
+import {
+  fetchCollections,
+  fetchCollection,
+  mergeCollections,
+} from "../api/fetchGroupCollection";
 
 const MEMBERS = ["donutgamer", "Jagger84", "stevmachine"];
 
 type CollectionPageProps = {
-  collections: ICollection[];
-  boardgames: IItem[];
-  members: string[];
+  initialData: ICollection[];
 };
 
 export async function getStaticProps() {
-  const data = await fetchGroupCollection(MEMBERS);
-  const { boardgames, collections } = data;
+  const initialData = await fetchCollections(MEMBERS);
 
   return {
     props: {
-      boardgames,
-      collections,
+      initialData,
     },
   };
 }
 
-const Index: NextPage<CollectionPageProps> = (props) => {
-  const { data } = useQuery(
-    "collections",
-    () => fetchGroupCollection(props.members),
-    { initialData: props }
+const Index: NextPage<CollectionPageProps> = ({ initialData }) => {
+  const results = useQueries(
+    MEMBERS.map((member, index) => ({
+      queryKey: ["collection", member],
+      queryFn: () => fetchCollection(member),
+      initialData: initialData[index],
+      refetchOnWindowFocus: false,
+    }))
   );
+
+  const data = useMemo(
+    () => mergeCollections(results.map((result: any) => result?.data)),
+    [results]
+  );
+
   const defaultValues = useMemo(
     () => ({
       orderBy: "name_asc",
