fcce66e6d7f88f29bdba87505fea542ab9a56bc4
Esteban Campos <campos.esteban@gmail.com>
2021-05-31 15:35:27 +1000

    more blank pages

---
+++ src/pages/plays/add.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Box, Container } from "@chakra-ui/react";
+import { NextPage } from "next";
+import {
+  withAuthUser,
+  withAuthUserTokenSSR,
+  AuthAction,
+  useAuthUser,
+} from "next-firebase-auth";
+import Navbar from "../../components/Layout/Navbar";
+import Footer from "../../components/Layout/Footer";
+
+type AddPlayPageProps = {};
+
+const AddPlayPage: NextPage<AddPlayPageProps> = () => {
+  const AuthUser = useAuthUser();
+
+  return (
+    <Container height="100vh" maxWidth="100%">
+      <Navbar user={AuthUser} signOut={AuthUser.signOut} />
+      <Box mt={12}>Community profile</Box>
+      <Footer />
+    </Container>
+  );
+};
+
+export const getServerSideProps = withAuthUserTokenSSR({})(async () => {
+  return {
+    props: {},
+  };
+});
+
+export default withAuthUser<AddPlayPageProps>({
+  whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
+})(AddPlayPage);
