fd9262d9bdf7bacd2915c216dd206c3a16840732
stvmachine-kaluza <esteban.valenzuela@kaluza.com>
2025-09-15 09:31:16 +1000

    feat: using useQuery

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,17 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { AppProps } from "next/app";
 import theme from "../theme";
-const queryClient = new QueryClient();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      cacheTime: 10 * 60 * 1000, // 10 minutes
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
