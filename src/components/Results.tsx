53cb93773fb34654921dfbedca2443a430c6801f
stvmachine-kaluza <esteban.valenzuela@kaluza.com>
2025-09-15 11:26:36 +1000

    feat: using context and colors for each user

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  Box,
   Container,
   Heading,
   LinkBox,
@@ -8,11 +9,13 @@ import {
   ListItem,
   Text,
   Wrap,
+  HStack,
 } from "@chakra-ui/react";
 import { useFormContext } from "react-hook-form";
 import SortBar from "../components/SortBar";
 import { useSearch } from "../hooks/useSearch";
 import { IItem } from "../utils/types";
+import { useMembers } from "../contexts/MemberContext";
 
 import GameCard from "./GameCard";
 
@@ -25,6 +28,7 @@ const Results = React.memo(({ boardgames }: ResultsProps) => {
     keys: ["name.text"],
   });
   const { watch } = useFormContext();
+  const { getMemberData } = useMembers();
   const watchedMembers = watch("members");
   const checkedMembers = Object.keys(watchedMembers).reduce(
     (accum: string[], key: string) => {
@@ -42,19 +46,42 @@ const Results = React.memo(({ boardgames }: ResultsProps) => {
         <Heading fontSize={"2xl"} mb={10}>
           {`Displaying ${results.length} games owned for the following members:`}
           <List.Root>
-            {checkedMembers.map((member, index) => (
-              <ListItem key={`${member}_${index}`}>
-                <LinkBox>
-                  <LinkOverlay
-                    href={`https://boardgamegeek.com/user/${member}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Text color="tomato">{member}</Text>
-                  </LinkOverlay>
-                </LinkBox>
-              </ListItem>
-            ))}
+            <HStack gap={3} mt={4} flexWrap="wrap">
+              {checkedMembers.map((member, index) => {
+                const memberData = getMemberData(member);
+                if (!memberData) return null;
+                
+                return (
+                  <ListItem key={`${member}_${index}`}>
+                    <LinkBox>
+                      <LinkOverlay
+                        href={`https://boardgamegeek.com/user/${member}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <HStack gap={2}>
+                          <Box
+                            width="32px"
+                            height="32px"
+                            borderRadius="full"
+                            bg={memberData.color.bg}
+                            color={memberData.color.color}
+                            display="flex"
+                            alignItems="center"
+                            justifyContent="center"
+                            fontSize="sm"
+                            fontWeight="bold"
+                          >
+                            {memberData.initial}
+                          </Box>
+                          <Text fontWeight="medium">{member}</Text>
+                        </HStack>
+                      </LinkOverlay>
+                    </LinkBox>
+                  </ListItem>
+                );
+              })}
+            </HStack>
           </List.Root>
         </Heading>
       ) : (
