5e5e12f80fdb3abb51ff25c52adc1213b03f5410
Esteban Campos <campos.esteban@gmail.com>
2025-09-25 23:36:44 +1000

    feat: fixed shared button

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,6 +1,6 @@
-import { Box, Flex, HStack } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { FiMenu, FiShare2 } from "react-icons/fi";
+import { FiMenu } from "react-icons/fi";
 
 import Logo from "./Logo";
 
@@ -10,7 +10,8 @@ type NavbarProps = {
 
 const Navbar: React.FC<NavbarProps> = ({ onMobileMenuOpen }) => {
   const router = useRouter();
-  const isHomepage = router.pathname === '/';
+  const isHomepage = router.pathname === "/";
+
   return (
     <Box
       position="sticky"
@@ -51,55 +52,14 @@ const Navbar: React.FC<NavbarProps> = ({ onMobileMenuOpen }) => {
           color="gray.700"
           _hover={{
             bg: "gray.100",
-            color: "gray.800"
+            color: "gray.800",
           }}
           _active={{
             bg: "gray.200",
-            color: "gray.900"
+            color: "gray.900",
           }}
         >
           <FiMenu size="24px" />
-        </Box>
-
-        <Box ml="auto" display={{ base: "none", md: "flex" }} alignItems="center">
-          {!isHomepage && (
-          <HStack gap={6} ml={6} display={{ base: "none", md: "flex" }}>
-            <Box
-              as="button"
-              display="inline-flex"
-              alignItems="center"
-              justifyContent="center"
-              px={3}
-              py={1}
-              ml={4}
-              fontSize="sm"
-              fontWeight="medium"
-              color="blue.500"
-              bg="white"
-              border="1px solid"
-              borderColor="blue.500"
-              borderRadius="md"
-              _hover={{
-                bg: "blue.50"
-              }}
-              onClick={() => {
-                if (navigator.share) {
-                  navigator.share({
-                    title: 'My Board Game Collection',
-                    text: 'Check out my board game collection!',
-                    url: window.location.href,
-                  }).catch(console.error);
-                } else {
-                  navigator.clipboard.writeText(window.location.href);
-                  alert('Link copied to clipboard!');
-                }
-              }}
-            >
-              <Box as={FiShare2} mr={2} />
-              Share Collection
-            </Box>
-          </HStack>
-          )}
         </Box>
       </Flex>
     </Box>
